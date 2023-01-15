a0fedd6b956bb19a4204e0fbb063fa66441e05af
jon brookes <marshyon@gmail.com>
2023-01-15 20:47:03 +0000

    initial nested navigation

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const OnboardingScreen = () => {
+    return (
+      <View>
+        <Text>onboardingScreen</Text>
+      </View>
+    )
+  }
